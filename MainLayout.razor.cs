2b7e4dd97e01d65fe4491fb26f0c53763ffb69e9
Swizzy <swizzy@xeupd.com>
2022-12-26 01:15:50 +0100

    Added MudBlazor + Theme switching support

---
+++ MainLayout.razor.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Components;
+using Blazored.LocalStorage;
+using MudBlazor;
+using Microsoft.JSInterop;
+
+namespace ZENBitPackToolbox
+{
+    public partial class MainLayout
+    {
+        private const string THEME_KEY = "theme";
+        private const string DARK_THEME = "dark";
+        private const string LIGHT_THEME = "light";
+
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
+
+        [Inject]
+        IJSRuntime JsRuntime { get; set; }
+
+        private MudThemeProvider? _mudThemeProvider;
+        private string ModeIcon => _isDarkMode ? Icons.Outlined.LightMode : Icons.Outlined.DarkMode;
+        private string ModeTitle => _isDarkMode ? "Switch to light theme" : "Switch to dark theme";
+        private bool _isDarkMode;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (await LocalStorage.ContainKeyAsync(THEME_KEY))
+            {
+                _isDarkMode = await LocalStorage.GetItemAsStringAsync(THEME_KEY) == DARK_THEME;
+            }
+            else
+            {
+                _isDarkMode = await _mudThemeProvider!.GetSystemPreference();
+                SaveTheme();
+            }
+            await base.OnInitializedAsync();
+        }
+
+        private void ToggleDarkMode()
+        {
+            _isDarkMode = !_isDarkMode;
+            SaveTheme();
+        }
+
+        private void SaveTheme()
+        {
+            var theme = _isDarkMode ? DARK_THEME : LIGHT_THEME;
+            LocalStorage.SetItemAsStringAsync(THEME_KEY, theme);
+            JsRuntime.InvokeVoidAsync("SetTheme", theme);
+        }
+    }
+}
---
+++ wwwroot/scripts/theme.js
@@ -0,0 +1,13 @@
+﻿window.SetTheme = function (currentTheme) {
+    console.log("Current Theme: " + currentTheme);
+    const classList = document.body.classList;
+    if (currentTheme == "dark") {
+        classList.add("dark-theme");
+        classList.remove("light-theme");
+
+    } else if (currentTheme == "light") {
+        classList.add("light-theme");
+        classList.remove("dark-theme");
+    }
+};
+SetTheme(localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
