b02010e38d8ee68a5e0e0e92a2039d620bb91bb6
Swizzy <swizzy@xeupd.com>
2022-12-29 23:28:15 +0100

    Change default snackbar position to bottom left

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using ZENBitPackToolbox.Managers;
 
@@ -24,7 +25,10 @@ public static class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        builder.Services.AddMudServices();
+        builder.Services.AddMudServices(options =>
+                                        {
+                                            options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomStart;
+                                        });
         builder.Services.AddBlazoredLocalStorage();
         builder.Services.AddScoped<StateManager>();
 
